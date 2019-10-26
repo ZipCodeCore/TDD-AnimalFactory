de6eeefc4da7cdb1fa99726b07310e2c92b236df
Kai Shields <kai@zipcoders-MacBook-Pro-3.local>
2019-10-26 18:36:27 -0400

    changes made

--- src/test/java/rocks/zipcodewilmington/DogHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/DogHouseTest.java
@@ -1,5 +1,7 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import rocks.zipcodewilmington.animals.Dog;
 import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
@@ -11,24 +13,104 @@ import java.util.Date;
  * @author leon on 4/19/18.
  */
 public class DogHouseTest {
-    // TODO - Create tests for `void add(Dog dog)`
-    // TODO - Create tests for `void remove(Integer id)`
-    // TODO - Create tests for `void remove(Dog dog)`
-    // TODO - Create tests for `Dog getDogById(Integer id)`
     // TODO - Create tests for `Integer getNumberOfDogs()`
 
     @Test
-    public void testGetNumberOfDogs() {
-        // Given (some
-        String name = "Milo";
-        Date birthDate = new Date();
-        Dog animal = AnimalFactory.createDog(name, birthDate);
-        DogHouse.clear();
+    public void testRemoveDogMethod1() {
+        // given
+        int idOfDog = 0;
+        Dog inputDog = new Dog("", new Date(), idOfDog);
+        Dog expectedDog = null;
+        DogHouse.add(inputDog);
 
-        // When
-        DogHouse.add(animal);
+        // when
+        DogHouse.remove(inputDog);
+        Dog actualDog = DogHouse.getDogById(idOfDog);
 
-        // Then
-        DogHouse.getNumberOfDogs();
+        // then
+        Assert.assertEquals(expectedDog, actualDog);
+    }
+
+
+    @Test
+    public void testRemoveDogMethod2() {
+        // given
+        DogHouse house = new DogHouse();
+        int idOfDog = 10;
+        Dog inputDog = new Dog("Milo", new Date(), idOfDog);
+        Dog expectedDog = null;
+        house.add(inputDog);
+
+        // when
+        house.remove(inputDog);
+        Dog actualDog = house.getDogById(idOfDog);
+
+        // then
+        Assert.assertEquals(expectedDog, actualDog);
+    }
+
+    @Test
+    public void testRemoveMethod1() {
+        // given
+        DogHouse house = new DogHouse();
+        int idOfDog = 0;
+        Dog inputDog = new Dog("", new Date(), idOfDog);
+        Dog expectedDog = null;
+        house.add(inputDog);
+
+        // when
+        house.remove(idOfDog);
+        Dog actualDog = house.getDogById(idOfDog);
+
+        // then
+        Assert.assertEquals(expectedDog, actualDog);
+    }
+
+
+    @Test
+    public void testRemoveMethod2() {
+        // given
+        DogHouse house = new DogHouse();
+        int idOfDog = 10;
+        Dog inputDog = new Dog("", new Date(), idOfDog);
+        Dog expectedDog = null;
+        house.add(inputDog);
+
+        // when
+        house.remove(idOfDog);
+        Dog actualDog = house.getDogById(idOfDog);
+
+        // then
+        Assert.assertEquals(expectedDog, actualDog);
+    }
+
+    @Test
+    public void testAddMethod1() {
+        // given
+        DogHouse house = new DogHouse();
+        int idOfDog = 0;
+        Dog expectedDog = new Dog("", new Date(), idOfDog);
+
+        // when
+        house.add(expectedDog);
+        Dog actualDog = house.getDogById(idOfDog);
+
+        // then
+        Assert.assertEquals(expectedDog, actualDog);
+    }
+
+    @Test
+    public void testAddMethod2() {
+        // given
+        DogHouse house = new DogHouse();
+        int idOfDog = 10;
+        Dog expectedDog = new Dog("", new Date(), idOfDog);
+
+        // when
+        house.add(expectedDog);
+        Dog actualDog = house.getDogById(idOfDog);
+
+        // then
+        Assert.assertEquals(expectedDog, actualDog);
     }
 }
