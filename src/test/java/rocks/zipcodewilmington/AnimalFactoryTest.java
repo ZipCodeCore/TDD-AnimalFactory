bf433c708a8eed41db90c7c5c3d09db09ee03170
Eddie Morales <eddie@zipcoders-MacBook-Pro.local>
2019-10-28 16:56:25 -0400

    Updated

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,9 +1,26 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Animal;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+
 /**
  * @author leon on 4/19/18.
  */
 public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+    @Test
+       public void createDogTest() {
+        Dog dog = AnimalFactory.createDog(null, null);
+        Assert.assertTrue(dog instanceof Dog);
+    }
+    @Test
+        public void createCatTest() {
+        Cat cat = AnimalFactory.createCat(null, null);
+        Assert.assertTrue(cat instanceof Cat);
+    }
 }
