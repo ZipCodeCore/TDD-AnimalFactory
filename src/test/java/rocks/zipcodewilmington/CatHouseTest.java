f1a98a59b67963fbec4733956fb87ac30baf0d5a
jgiroso <jrgiroso@gmail.com>
2021-07-10 10:16:02 -0400

    part1 mostly complete

--- src/test/java/rocks/zipcodewilmington/CatHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/CatHouseTest.java
@@ -1,10 +1,21 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Test;
+
 /**
  * @author leon on 4/19/18.
  */
 public class CatHouseTest {
     // TODO - Create tests for `void add(Cat cat)`
+//    @Test
+//    public void catAddTest() {
+//        //given
+//        String givenName = "Kevin";
+//        Date giveBirthDate = new Date();
+//        Integer givenId = 0;
+//        //when
+//        Cat cat = new Cat(given)
+//    }
     // TODO - Create tests for `void remove(Integer id)`
     // TODO - Create tests for `void remove(Cat cat)`
     // TODO - Create tests for `Cat getCatById(Integer id)`
