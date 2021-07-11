8b612bacb74dbf4ed23f90407e4187e370fd64f8
TatianaDeAngelo <85692272+TatianaDeAngelo@users.noreply.github.com>
2021-07-11 12:56:42 -0400

    Dog and Cat tests passed

--- src/test/java/rocks/zipcodewilmington/CatHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/CatHouseTest.java
@@ -1,12 +1,60 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
 public class CatHouseTest {
-    // TODO - Create tests for `void add(Cat cat)`
+
     // TODO - Create tests for `void remove(Integer id)`
     // TODO - Create tests for `void remove(Cat cat)`
     // TODO - Create tests for `Cat getCatById(Integer id)`
+
+
+//    @Test
+//    public void getCatByIdTest() {
+//        //Given
+//        String givenName = "Chaco";
+//        Cat cat = new Cat ();
+//
+//        //When
+//        Cat actual = CatHouse.getCatById(givenId);
+//
+//        //Then
+//        Assert.assertEquals(cat, actual);
+//    }
+
+    // TODO - Create tests for `void add(Cat cat)`
+
+
     // TODO - Create tests for `Integer getNumberOfCats()`
-}
+
+    @Test
+    public void testGetNumberOfCats() {
+        // Given (some
+        String name = "Fluffy";
+        Date birthDate = new Date();
+        Cat animal = AnimalFactory.createCat(name, birthDate);
+        CatHouse.clear();
+
+        // When
+        CatHouse.add(animal);
+
+        // Then
+        CatHouse.getNumberOfCats();
+    }
+
+
+
+
+
+
+} // final brace
--- src/test/java/rocks/zipcodewilmington/DogHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/DogHouseTest.java
@@ -11,10 +11,24 @@ import java.util.Date;
  * @author leon on 4/19/18.
  */
 public class DogHouseTest {
-    // TODO - Create tests for `void add(Dog dog)`
+
     // TODO - Create tests for `void remove(Integer id)`
     // TODO - Create tests for `void remove(Dog dog)`
     // TODO - Create tests for `Dog getDogById(Integer id)`
+
+
+    // TODO - Create tests for `void add(Dog dog)`
+
+    @Test
+    public void addDogTest() {
+        //Given
+        //When
+        //Then
+    }
+
+
+
+
     // TODO - Create tests for `Integer getNumberOfDogs()`
 
     @Test
