a0529249583bccd7b5ac2c796bc6269c85217823
junior <junior@zipcodes-MacBook-Pro-5.local>
2021-11-04 12:22:18 -0400

    finished CatTest and DogTest

--- src/main/java/rocks/zipcodewilmington/animals/Mammal.java
+++ src/main/java/rocks/zipcodewilmington/animals/Mammal.java
@@ -14,12 +14,15 @@ public abstract class Mammal implements Animal {
     private String name;
     private Date birthDate;
 
+
+
     public Mammal(String name, Date birthDate, Integer id) {
         this.name = name;
         this.birthDate = birthDate;
         this.eatenMeals = new ArrayList<>();
         this.id = id;
     }
+
 
     public String getName() {
         return name;
--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,9 +1,21 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Animal;
+
 /**
  * @author leon on 4/19/18.
  */
 public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
+    @Test
+    public  void createDog(){
+        //Given
+
+
+      //When
+
+        //Then
+    }
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
 }
