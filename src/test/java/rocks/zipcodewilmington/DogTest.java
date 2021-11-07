e9ba30cfe66fdb71fa9a3626b7b57c704c38d814
Keerthana Srinivasan <keerthihamsa.06@gmail.com>
2021-11-07 00:05:41 -0400

    Completed lab

--- src/test/java/rocks/zipcodewilmington/DogTest.java
+++ src/test/java/rocks/zipcodewilmington/DogTest.java
@@ -2,7 +2,11 @@ package rocks.zipcodewilmington;
 
 import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Animal;
 import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.Mammal;
+
+import java.util.Date;
 
 /**
  * @author leon on 4/19/18.
@@ -28,4 +32,61 @@ public class DogTest {
         String dogName = dog.getName();
         Assert.assertEquals(dogName, givenName);
     }
+
+    @Test
+    public void test_new_dog() {
+        Dog d = new Dog("Tom", new Date(), 1);
+        d.setName("Tom");
+        Assert.assertEquals("Tom", d.getName());
+
+    }
+
+    @Test
+    public void test_speak() {
+        Dog d = new Dog("Tom", new Date(), 2);
+        String dd = d.speak();
+        Assert.assertEquals("bark!", dd);
+    }
+
+    @Test
+    public void test_set_DateOfBirth() {
+        Dog d = new Dog("Tom", new Date(), 2);
+        Date e = new Date();
+        d.setBirthDate(e);
+        Assert.assertEquals(e, d.getBirthDate());
+    }
+
+    @Test
+    public void test_eat() {
+        Dog d = new Dog("Tom", new Date(), 2);
+        Food food = new Food();
+        d.eat(food);
+        Assert.assertEquals(new Integer(1), d.getNumberOfMealsEaten());
+    }
+
+    @Test
+    public void test_getId() {
+
+        Dog d = new Dog("Tom", new Date(), 2);
+        d.getId();
+        Assert.assertEquals(2, d.getId().intValue());
+    }
+
+
+    @Test
+    public void animalTest() {
+        Dog d = new Dog("doggy", new Date(), 2);
+        Assert.assertEquals(true, d instanceof Animal);
+
+    }
+
+    @Test
+    public void mammalTest() {
+        Dog d = new Dog("doggy", new Date(), 2);
+        Assert.assertEquals(true, d instanceof Mammal);
+    }
+
+
 }
+
+
